e9e02eca11a3fd69ee62c39c1951942bacbb72fc
Meagan Waller <meaganewaller@gmail.com>
2021-03-06 18:39:12 -0500

    🎨 Add index page and about page

---
+++ src/components/indexLayout.js
@@ -0,0 +1,40 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import MainNav from "./mainNav"
+import Footer from "./footer"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <MainNav siteTitle={data.site.siteMetadata?.title || `Title`} />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
